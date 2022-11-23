342ab7a094301a174a3b0139c9b10ffbdaad8f4c
rowland1995 <71675410+rowland1995@users.noreply.github.com>
2022-11-23 16:19:38 -0600

    Added simple guessing game

---
+++ guess_computers_number.py
@@ -0,0 +1,20 @@
+import random
+
+while True:
+    computer_number = random.randint(1,5) #variable for random number
+    user_number = input("Guess the computers number between 1-5: ") #takes users guess
+    print("The computer picked : " + str(computer_number))
+    
+    if int(user_number) > 5:
+        print("Your guess is too high")
+        
+    if int(user_number) < 0:
+        print("Your guess is too low")
+
+    if int(user_number) == computer_number:
+        print("You picked correct!")
+        
+    else:
+        print("You chose wrong. Try again")
+
+
